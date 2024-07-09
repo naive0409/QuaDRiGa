53b6d6f427db52a764afc3f8851e428aa1fe53ce
Wu Sanmu <wus4nmu@gmail.com>
2024-07-09 16:56:57 +0800

    comment

--- test.m
+++ test.m
@@ -2,7 +2,7 @@ clear;
 close all;
 
 %% random number generator control
-% rng(20240705);
+rng(20240705);
 % disp(RandStream.getGlobalStream);
 
 %% constants
@@ -10,13 +10,13 @@ position_a = [0; 0; 25]; % alice
 position_b = [1; 0; 1.5]; % bob
 center_frequency = 3.7e9;
 update_rate = 0.01;
-no_sc = 64;
-sc_bw = 20e6;
+no_sc = 64; % subcarrier number
+sc_bw = 20e6; % subcarrier bandwidth
 
 %% a->b
 %% antenna
 a = qd_arrayant('dipole');
-a.normalize_gain(1,35);
+a.normalize_gain(1,35); % antenna gain
 % a.visualize();
 % a = qd_arrayant( 'parabolic', 3, center_frequency, [] , 3);       % Sat. antenna
 % a.center_frequency = center_frequency;
@@ -27,12 +27,12 @@ a.normalize_gain(1,35);
 % [directivity_dBi, gain_dBi] = a.calc_gain();
 
 %% alice track
-t_alice = qd_track('linear',0,0);
+t_alice = qd_track('linear',0,0); % 不动
 t_alice.initial_position = position_a;
 
 %% bob track
-t_bob = qd_track('linear', 0.75, 0); % 创建新轨迹
-t_bob.movement_profile = [0, 0.5; 0, 0.75]; % 1.5m/s
+t_bob = qd_track('linear', 0.75, 0); % 0.75m长
+t_bob.movement_profile = [0, 0.5; 0, 0.75]; % 速度1.5m/s
 t_bob.initial_position = position_b;
 
 %% plot distance & time
@@ -50,18 +50,18 @@ l.simpar.show_progress_bars = 0; % 禁用进度条指示器
 l.tx_track = t_alice;
 l.rx_track = t_bob;
 
-l.set_scenario('3GPP_38.901_UMa_NLOS'); % 设置场景为非视距NLOS，包含多径效应和阴影衰落
+l.set_scenario('3GPP_38.901_UMa_NLOS'); % 和论文一样
 
 l.tx_array = a; % 在两端使用相同的天线
 l.rx_array = a;
 
 l.update_rate = update_rate;
 
-% l.visualize();title('a->b');
+% l.visualize();title('a->b'); % 可视化
 
 %% generate channel coeff & frequency response
 c_initial = l.get_channels; % 计算信道系数
-fr_initial = c_initial.fr(no_sc*sc_bw,no_sc); % no_rx no_tx no_subcarrier no_snapshot
+fr_initial = c_initial.fr(no_sc*sc_bw,no_sc); % frequency response : no_rx no_tx no_subcarrier no_snapshot
 % disp("size of c_initial.coeff:")
 % disp(size(c_initial.coeff));
 % pow = 10*log10(reshape(sum(abs(c_initial.coeff(:,:,:,:)).^2,3),2,[]));
@@ -76,7 +76,7 @@ l.simpar.show_progress_bars = 0; % 禁用进度条指示器
 l.tx_track = t_bob;
 l.rx_track = t_alice;
 
-l.set_scenario('3GPP_38.901_UMa_NLOS'); % 设置场景为非视距NLOS，包含多径效应和阴影衰落
+l.set_scenario('3GPP_38.901_UMa_NLOS');
 
 l.tx_array = a; % 在两端使用相同的天线
 l.rx_array = a;
@@ -117,9 +117,6 @@ figure;
 set(gcf,'Position',[1100 100 1000 1000]);
 for i = 1:num_snapshots
   snapshot = snapshots_to_plot(i);
-  % 提取初始信道系数和反向信道系数
-  h_initial = c_initial.coeff(:, :, :, snapshot);
-  h_reversed = c_reversed.coeff(:, :, :, snapshot);
   % 创建子图
   subplot(2, 2, i);
   plot(reshape(fr_initial(:,:,:,i),1,[]),'o','DisplayName', 'Initial(a->b)');
